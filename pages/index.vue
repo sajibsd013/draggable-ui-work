726ad370de847318fadf477a461d8e3f26753ea4
sajibsd013 <sajibsd013@gmail.com>
2023-11-07 20:49:17 +0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="card my-5 shadow-lg py-5">
+      <div class="card-body">
+        <h1 class="text-center">John Doe</h1>
+        <Test/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+</style>
