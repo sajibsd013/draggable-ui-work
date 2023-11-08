25675873c904eb567c27367e090e2ea29e6eb506
sajibsd013 <sajibsd013@gmail.com>
2023-11-08 21:32:21 +0600

    add draggable info block

--- components/InfoBlock.vue
+++ components/InfoBlock.vue
@@ -15,47 +15,47 @@
       </div>
       <div class="row g-5">
         <div class="col-md-6">
-          <template
-            v-for="(option, index) in block_data.left_options"
-            :key="index"
-          >
-            <div
-              class="row my-1 block block__inner p-1 rounded justify-content-center"
-            >
-              <div class="col-12">
-
-                <font-awesome-icon
-                  icon="fa-solid fa-xmark"
-                  class="text-white pointer inner_action__btn inner_trash__btn bg-danger rounded-circle"
-                />
-              </div>
-              <div class="col-md-5 d-flex justify-content-start aling-items-center">
-                <font-awesome-icon
-                  icon="fa-solid fa-bars "
-                  class="text-secondary pointer inner_action__btn inner_move__btn"
-                />
-                <textarea
-                  type="text"
-                  class="form-control form-control-sm border-0 bg-white fw-semibold"
-                  id="title"
-                  v-model="block_data.left_options[index].label"
-                  rows="1"
+          <draggable v-model="block_data.left_options" item-key="id">
+            <template #item="{ element }">
+              <div
+                class="row my-1 block block__inner p-1 rounded justify-content-center"
+              >
+              <!-- {{ element }} -->
+                <div class="col-12">
+                  <font-awesome-icon
+                    icon="fa-solid fa-xmark"
+                    class="text-white pointer inner_action__btn inner_trash__btn bg-danger rounded-circle"
+                  />
+                </div>
+                <div
+                  class="col-md-5 d-flex justify-content-start aling-items-center"
                 >
-                </textarea>
+                  <font-awesome-icon
+                    icon="fa-solid fa-bars "
+                    class="text-secondary pointer inner_action__btn inner_move__btn"
+                  />
+                  <textarea
+                    type="text"
+                    class="form-control form-control-sm border-0 bg-white fw-semibold"
+                    id="title"
+                    v-model="element.label"
+                    rows="1"
+                  >
+                  </textarea>
+                </div>
+                <div class="col-md-7">
+                  <textarea
+                    type="text"
+                    class="form-control form-control-sm border-0 bg-white small"
+                    id="title"
+                    v-model="element.information"
+                    rows="1"
+                  >
+                  </textarea>
+                </div>
               </div>
-              <div class="col-md-7">
-                <textarea
-                  type="text"
-                  class="form-control form-control-sm border-0 bg-white small"
-                  id="title"
-                  v-model="block_data.left_options[index].information"
-                  rows="1"
-                >
-                </textarea>
-              </div>
-            </div>
-          </template>
-
+            </template>
+          </draggable>
           <button class="btn w-100 add_btn my-3">
             <font-awesome-icon
               icon="fa-solid fa-plus"
@@ -65,47 +65,47 @@
           </button>
         </div>
         <div class="col-md-6">
-          <template
-            v-for="(option, index) in block_data.right_options"
-            :key="index"
-          >
-            <div
-              class="row my-2 block block__inner p-1 rounded justify-content-center"
-            >
-              <div class="col-12">
-
-                <font-awesome-icon
-                  icon="fa-solid fa-xmark"
-                  class="text-white pointer inner_action__btn inner_trash__btn bg-danger rounded-circle"
-                />
-              </div>
-              <div class="col-md-5 d-flex justify-content-start aling-items-center">
-                <font-awesome-icon
-                  icon="fa-solid fa-bars "
-                  class="text-secondary pointer inner_action__btn inner_move__btn"
-                />
-                <textarea
-                  type="text"
-                  class="form-control form-control-sm border-0 bg-white fw-semibold"
-                  id="title"
-                  v-model="block_data.right_options[index].label"
-                  rows="1"
+          <draggable v-model="block_data.right_options" item-key="id">
+            <template #item="{ element }">
+              <div
+                class="row my-1 block block__inner p-1 rounded justify-content-center"
+              >
+              <!-- {{ element }} -->
+                <div class="col-12">
+                  <font-awesome-icon
+                    icon="fa-solid fa-xmark"
+                    class="text-white pointer inner_action__btn inner_trash__btn bg-danger rounded-circle"
+                  />
+                </div>
+                <div
+                  class="col-md-5 d-flex justify-content-start aling-items-center"
                 >
-                </textarea>
+                  <font-awesome-icon
+                    icon="fa-solid fa-bars "
+                    class="text-secondary pointer inner_action__btn inner_move__btn"
+                  />
+                  <textarea
+                    type="text"
+                    class="form-control form-control-sm border-0 bg-white fw-semibold"
+                    id="title"
+                    v-model="element.label"
+                    rows="1"
+                  >
+                  </textarea>
+                </div>
+                <div class="col-md-7">
+                  <textarea
+                    type="text"
+                    class="form-control form-control-sm border-0 bg-white small"
+                    id="title"
+                    v-model="element.information"
+                    rows="1"
+                  >
+                  </textarea>
+                </div>
               </div>
-              <div class="col-md-7">
-                <textarea
-                  type="text"
-                  class="form-control form-control-sm border-0 bg-white small"
-                  id="title"
-                  v-model="block_data.right_options[index].information"
-                  rows="1"
-                >
-                </textarea>
-              </div>
-            </div>
-          </template>
-
+            </template>
+          </draggable>
           <button class="btn w-100 add_btn my-3">
             <font-awesome-icon
               icon="fa-solid fa-plus"
@@ -114,13 +114,18 @@
             />
           </button>
         </div>
+   
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import draggable from "vuedraggable";
 export default {
+  components: {
+    draggable,
+  },
   data() {
     return {
       block_data: {
--- components/Test.vue
+++ components/Test.vue
@@ -1,46 +1,43 @@
 <template>
-    <div>
-      <h3>Transition Example</h3>
-      <draggable
-        v-model="myList" item-key="id"
-      >
-      <template #item="{element}">
-          <div>
-            <p>
-              {{element.name}} 
-            </p>
-            <p>{{element.order}}</p>
-          </div>
+  <div>
+    <h3>Transition Example</h3>
+    <draggable v-model="myList" item-key="id">
+      <template #item="{ element }">
+        <div>
+          <p>
+            {{ element.name }}
+          </p>
+        </div>
       </template>
-      </draggable>
-    </div>
-  </template>
-  
-  <script>
-  import draggable from "vuedraggable";
-  
-  export default {
-    components: {
-      draggable,
-    },
-    data() {
-      return {
-        myList: [
-          { name: "Third Item", address:"Sylhet", order: 3 },
-          { name: "First Item", address:"Sylhet1", order: 1 },
-          { name: "Second Item", address:"Sylhet2", order: 2 },
-        ],
-      };
-    },
-  };
-  </script>
-  
-  <style>
-  .flip-transition-move {
-    transition: all 0.7s;
-  }
-  .list-item{
-    padding: 10px;
-    border: 1px solid #ccc;
-  }
-  </style>
+    </draggable>
+  </div>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      myList: [
+        { name: "Third Item", address: "Sylhet" },
+        { name: "First Item", address: "Sylhet1" },
+        { name: "Second Item", address: "Sylhet2"},
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.flip-transition-move {
+  transition: all 0.7s;
+}
+.list-item {
+  padding: 10px;
+  border: 1px solid #ccc;
+}
+</style>
